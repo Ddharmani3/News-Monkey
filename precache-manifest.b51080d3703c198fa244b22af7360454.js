ce795550d1cf3fbedbeb5e9ac5455ac5e6b9f14d
Hardik Dharmani <Ddharmani99@gmail.com>
2022-08-13 00:14:49 +0530

    Updates

---
+++ precache-manifest.b51080d3703c198fa244b22af7360454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "163371786aca6567a750",
+    "url": "/News-Monkey/static/css/main.6ab6cbdc.chunk.css"
+  },
+  {
+    "revision": "163371786aca6567a750",
+    "url": "/News-Monkey/static/js/main.d80331e0.chunk.js"
+  },
+  {
+    "revision": "24397f996f6a594a938c",
+    "url": "/News-Monkey/static/js/runtime~main.51b9b110.js"
+  },
+  {
+    "revision": "f40463bc592801d27a7a",
+    "url": "/News-Monkey/static/js/2.a72ea685.chunk.js"
+  },
+  {
+    "revision": "408621d3a15eaf977c3c",
+    "url": "/News-Monkey/static/js/3.87f4fc67.chunk.js"
+  },
+  {
+    "revision": "caf528fb6ef4aa685cc2f0d52d7d1f08",
+    "url": "/News-Monkey/index.html"
+  }
+];
