c234431b3b4e4e9ed41ed3b3a3459f18a0ef395e
Hardik Dharmani <Ddharmani99@gmail.com>
2022-08-16 11:37:27 +0530

    tried another basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={window.location.pathname || ''}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
     </HashRouter>
   </React.StrictMode>
