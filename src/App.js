9c85c5cf3c3b0eb84c4cd7822c0760748dda5dbe
Hardik Dharmani <Ddharmani99@gmail.com>
2022-08-16 11:16:40 +0530

    routes working on local host

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import NavBar from './components/navbar';
 import News from './components/news';
 import React, { Component } from 'react'
 import {
-  Route
+  Route,
+  Routes
 } from "react-router-dom";
 // import { NavHashLink } from 'react-router-hash-link';
 
@@ -17,7 +18,21 @@ export default class App extends Component {
 
 
           <NavBar />
+
+
+          <Routes>
+              <Route exact path="/"element={<News key="home" pageSize={this.pageSize} country="in" category="general" />}/>
+              <Route exact path="/business"element={<News key="business" pageSize={this.pageSize} country="in" category="business" />}/>
+              <Route exact path="/entertainment"element={<News key="entertaiment" pageSize={this.pageSize} country="in" category="entertainment" />}/>
+              <Route exact path="/health"element={<News key="health" pageSize={this.pageSize} country="in" category="health" />}/>
+              <Route exact path="/sports"element={<News key="sports" pageSize={this.pageSize} country="in" category="sports" />}/>
+              <Route exact path="/science"element={<News key="science" pageSize={this.pageSize} country="in" category="science" />}/>
+              <Route exact path="/technology"element={<News key="technology" pageSize={this.pageSize} country="in" category="technology" />}/>
+            </Routes>
+
+
           {/* <News key="home" pageSize={this.pageSize} country="in" category="general" /> */}
+          {/* <Routes>
           <Route exact path="/"> <News key="home" pageSize={this.pageSize} country="in" category="general" /></Route>
           <Route path="/business"> <News key="business" pageSize={this.pageSize} country="in" category="business" /></Route>
           <Route path="/entertaiment"> <News key="entertaiment" pageSize={this.pageSize} country="in" category="entertainment" /></Route>
@@ -25,6 +40,7 @@ export default class App extends Component {
           <Route path="/sports"> <News key="sports" pageSize={this.pageSize} country="in" category="sports" /></Route>
           <Route path="/science"> <News key="science" pageSize={this.pageSize} country="in" category="science" /></Route>
           <Route path="/technology"> <News key="technology" pageSize={this.pageSize} country="in" category="technology" /></Route>
+          </Routes> */}
 
 
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-// import { NavLink } from 'react-router-hash-link';
+// import { Link } from 'react-router-hash-link';
 
 export class NavBar extends Component {
   static propTypes = {}
@@ -20,22 +20,22 @@ export class NavBar extends Component {
       <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
           <div className="container-fluid">
-            <NavLink className="navbar-brand" exact to="/">News Monkey</NavLink>
+            <Link className="navbar-brand" exact to="/">News Monkey</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <NavLink className="nav-link active" aria-current="page" exact to="/">Home</NavLink>
+                  <Link className="nav-link active" aria-current="page" exact to="/">Home</Link>
                 </li>
                 
-                <li className="nav-item"><NavLink className="nav-link" exact to="/business">Business</NavLink></li>
-                <li className="nav-item"><NavLink className="nav-link" exact to="/ent">Entertainment</NavLink></li>
-                <li className="nav-item"><NavLink className="nav-link" exact to="#hlth">Health</NavLink></li>
-                <li className="nav-item"><NavLink className="nav-link" exact to="#spo">Sports</NavLink></li>
-                <li className="nav-item"><NavLink className="nav-link" exact to="#sci">Science</NavLink></li>
-                <li className="nav-item"><NavLink className="nav-link" exact to="#tech">Technology</NavLink></li>
+                <li className="nav-item"><Link className="nav-link" exact to="/business">Business</Link></li>
+                <li className="nav-item"><Link className="nav-link" exact to="/entertainment">Entertainment</Link></li>
+                <li className="nav-item"><Link className="nav-link" exact to="/health">Health</Link></li>
+                <li className="nav-item"><Link className="nav-link" exact to="/sports">Sports</Link></li>
+                <li className="nav-item"><Link className="nav-link" exact to="/science">Science</Link></li>
+                <li className="nav-item"><Link className="nav-link" exact to="/technology">Technology</Link></li>
 
                 {/* <li className="nav-item">
                 <a className="nav-link disabled">Disabled</a>
@@ -45,14 +45,14 @@ export class NavBar extends Component {
 
               </ul>
               {/* <div className="dropdown mx-1">
-                <NavLink className="btn btn-primary dropdown-toggle " to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link className="btn btn-primary dropdown-toggle " to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   {"Country"}
-                </NavLink> */}
+                </Link> */}
 
                 {/* <ul className="dropdown-menu">
-                  <li><NavLink className="dropdown-item" to="/" onClick={() => { this.setState = { country: "in" } }}>India</NavLink></li>
-                  <li><NavLink className="dropdown-item" to="/" onClick={() => { this.setState = { country: "us" } }}>US</NavLink></li>
-                  <li><NavLink className="dropdown-item" to="/" onClick={() => { this.setState = { country: "" } }}>World</NavLink></li>
+                  <li><Link className="dropdown-item" to="/" onClick={() => { this.setState = { country: "in" } }}>India</Link></li>
+                  <li><Link className="dropdown-item" to="/" onClick={() => { this.setState = { country: "us" } }}>US</Link></li>
+                  <li><Link className="dropdown-item" to="/" onClick={() => { this.setState = { country: "" } }}>World</Link></li>
                 </ul> */}
               {/* </div> */}
               <form className="d-flex" role="search">
