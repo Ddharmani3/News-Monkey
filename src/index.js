e1f5a1da4c1c77eaeceedf6da497d25e8b8e6f8a
Hardik Dharmani <Ddharmani99@gmail.com>
2022-08-16 11:41:23 +0530

    changed hashrouter to browserrouter

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
-  HashRouter
+  BrowserRouter
 
 } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
