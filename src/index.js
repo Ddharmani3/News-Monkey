569b0ebe15ec2e4c49657915d9740d293ffea76c
Hardik Dharmani <Ddharmani99@gmail.com>
2022-08-16 11:26:31 +0530

    tried using basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/News-Monkey">
     <App />
     </HashRouter>
   </React.StrictMode>
